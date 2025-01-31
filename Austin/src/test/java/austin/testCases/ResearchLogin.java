2d48f8a19e1a865baa61b870989620d0431be421
VijayVarala <varalavijay18@gmail.com>
2025-01-31 13:07:38 +0530

    update

--- Austin/src/test/java/austin/testCases/ResearchLogin.java
+++ Austin/src/test/java/austin/testCases/ResearchLogin.java
@@ -42,7 +42,7 @@ public class ResearchLogin extends BaseClass
 
 	private void handleLoginFailure() throws IOException 
 	{
-		
+		System.out.println("Manoranjan");
 		logger.info("Research login failed");
 		Capture.CaptureScreenshot(driver, "img.png");
 		Assert.assertTrue(false);
